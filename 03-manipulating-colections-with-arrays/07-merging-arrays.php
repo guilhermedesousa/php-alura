c0e4ab715749ad84ab926f84f6514cfff4dca7a9
Guilherme de Sousa <guilhermedesousa.dev@gmail.com>
2022-07-01 18:51:23 -0300

    Manipulate array colections

--- 03-manipulating-colections-with-arrays/07-merging-arrays.php
+++ 03-manipulating-colections-with-arrays/07-merging-arrays.php
@@ -1,5 +1,7 @@
 <?php
 
+use PHP_CodeSniffer\Tokenizers\PHP;
+
 $students_2021 = [
     'Ana',
     'João',
@@ -20,3 +22,16 @@ $new_students = [
 $students_2022 = array_merge($students_2021, $new_students);
 var_dump($students_2022);
 var_dump($students_2021 + $new_students);
+
+// "unpacking" operator
+var_dump([...$students_2021, 'Pedro', ...$new_students]);
+
+// "spread" operator
+spread_op(...[1, 2, 3]);
+
+function spread_op(int $a, int $b, int $c)
+{
+    echo $a . PHP_EOL;
+    echo $b . PHP_EOL;
+    echo $c . PHP_EOL;
+}
